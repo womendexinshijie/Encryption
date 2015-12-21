4a709bf4d40f2ec0cfb4c59a58e49d14bfcb105d
junhaoshen <junhaoshen@junhaoshendemac-mini.local>
2015-12-21 17:10:24 +0800

    Initial Commit

---
+++ Encryption/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Encryption
+//
+//  Created by junhaoshen on 15/12/21.
+//  Copyright © 2015年 junhaoshen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
